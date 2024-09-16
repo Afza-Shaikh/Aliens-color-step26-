b9831be1a6545957a4a4fb2c4b5d8a09c0b631f9
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:27:20 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,20 @@
+let varaible_alien = "green";
+//block run
+//if (varaible_alien === 'green'){
+   // console.log(`player just earn 5 point for shooting the alien!`)
+
+//}
+//else{
+   // console.log(`player just earn 5 point.`)
+
+//}
+//else block run
+varaible_alien = "red"
+if (varaible_alien === 'green'){
+     console.log(`player just earn 10 point for shooting the alien!`)
+ 
+ }
+ else{
+     console.log(`player just earn 10 point.`)
+ }
+
